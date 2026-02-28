fa29136f90f0af8b162b40e971910ed388c5021b
Abhishek mahato <professionalmahito@gmail.com>
2026-02-28 13:00:08 +0530

    Change max_new_tokens from 30 to 20

--- main.py
+++ main.py
@@ -15,13 +15,14 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+device = "cpu"
 # Load model once at startup
 processor = BlipProcessor.from_pretrained(
     "Salesforce/blip-image-captioning-base"
 )
 model = BlipForConditionalGeneration.from_pretrained(
     "Salesforce/blip-image-captioning-base"
-)
+).to(device)
 
 @app.post("/caption")
 async def caption(file: UploadFile = File(...)):
@@ -29,7 +30,7 @@ async def caption(file: UploadFile = File(...)):
     image = Image.open(io.BytesIO(contents)).convert("RGB")
 
     inputs = processor(image, return_tensors="pt")
-    out = model.generate(**inputs, max_new_tokens=30)
+    out = model.generate(**inputs, max_new_tokens=20)
 
     caption = processor.decode(out[0], skip_special_tokens=True)
 
